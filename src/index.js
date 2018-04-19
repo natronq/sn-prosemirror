a3e29cd695c95efd4bc15c5add70694fa37a298c
natronq <yannick@moravit.com>
2018-04-20 00:26:52 +0200

    Add Metadata

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-// MarkdownView{
-class MarkdownView {
-  constructor(target, content) {
-    this.textarea = target.appendChild(document.createElement('textarea'))
-    this.textarea.value = content
-  }
-
-  get content() {
-    return this.textarea.value
-  }
-  focus() {
-    this.textarea.focus()
-  }
-  destroy() {
-    this.textarea.remove()
-  }
-}
-// }
-
 // ProseMirrorView{
 import { EditorView } from 'prosemirror-view'
 import { EditorState } from 'prosemirror-state'
@@ -50,7 +31,6 @@ class ProseMirrorView {
 // }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  var place = document.querySelector('#editor')
   var workingNote
   var view
 
@@ -74,7 +54,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
       return
     }
 
-    view = new ProseMirrorView(place, note.content.text)
+    if (view) {
+      view.destroy()
+    }
+
+    view = new ProseMirrorView(
+      document.querySelector('#editor'),
+      note.content.text
+    )
   })
 
   editor.addEventListener('input', function(event) {
